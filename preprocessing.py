eae36341389505caf7e88e21d566e6537e441fda
Lucas Cavalcante <lcdaponte@gmail.com>
2023-05-29 16:26:10 -0300

    feat: added sklearn module and it helped with de missing data

--- preprocessing.py
+++ preprocessing.py
@@ -8,3 +8,10 @@ Y = dataset.iloc[:, -1]
 
 print(X)
 print(Y)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X.iloc[:, 1:3])
+X.iloc[:, 1:3] = imputer.transform(X.iloc[:, 1:3])
+
+print(X)
