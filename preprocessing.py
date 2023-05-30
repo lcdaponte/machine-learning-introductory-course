12c20b7e305226d4cb50951b5fdbb6e0ef54f14d
Lucas Cavalcante <lcdaponte@gmail.com>
2023-05-30 09:56:47 -0300

    fix: fixed a bug while reading the csv

--- preprocessing.py
+++ preprocessing.py
@@ -3,15 +3,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1]
-Y = dataset.iloc[:, -1]
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
 
 print(X)
 print(Y)
 
-from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(X.iloc[:, 1:3])
-X.iloc[:, 1:3] = imputer.transform(X.iloc[:, 1:3])
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 print(X)
