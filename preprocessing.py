c00210a339e7ee206e7334f499db9a2c77f2a575
Lucas Cavalcante <lcdaponte@gmail.com>
2023-05-30 10:27:02 -0300

    feat: encoded the dependent variable

--- preprocessing.py
+++ preprocessing.py
@@ -1,16 +1,21 @@
+#importing necessary modules
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
+#reading from dataset and splitting between independent and dependet variables
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-Y = dataset.iloc[:, -1].values
+y = dataset.iloc[:, -1].values
 
 print(X)
-print(Y)
+print(y)
+
+#replacing the missing values with the average of the other values
 
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, 1:3])
@@ -18,6 +23,14 @@ X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 print(X)
 
+#encoding the categorical data within the independent variables
+
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 print(X)
+
+#encoding the depedent variable
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(y)
