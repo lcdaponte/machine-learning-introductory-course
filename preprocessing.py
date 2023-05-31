8f0275c65bb06d45dc65dacefa5ed043eb3a55b1
Lucas Cavalcante <lcdaponte@gmail.com>
2023-05-31 15:45:44 -0300

    feat: splitted dataset between training set and test set

--- preprocessing.py
+++ preprocessing.py
@@ -6,14 +6,12 @@ from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split 
 
 #reading from dataset and splitting between independent and dependet variables
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-
-print(X)
-print(y)
 
 #replacing the missing values with the average of the other values
 
@@ -21,16 +19,19 @@ imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-print(X)
-
 #encoding the categorical data within the independent variables
 
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
-print(X)
 
 #encoding the depedent variable
 le = LabelEncoder()
 y = le.fit_transform(y)
 
-print(y)
+#split the dataset into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
