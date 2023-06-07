477af0fd1f854eb8ce83222b521f3e354f5c4953
Lucas Cavalcante <lcdaponte@gmail.com>
2023-06-07 19:51:44 -0300

    feat: Predicting the Test set results

--- simple-linear-regression/simple_linear_regression.py
+++ simple-linear-regression/simple_linear_regression.py
@@ -2,6 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
@@ -9,5 +11,13 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Training the Simple Linear Regression model on the Training set
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+print(y_pred)
